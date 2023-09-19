d24bf8d0d5e118a5a1e4f1901f1b5c62d8a08f8e
Yash <thisisyashs@gmail.com>
2023-09-19 18:31:39 +0530

    Client side Routing

--- next-routing/src/pages/product/index.tsx
+++ next-routing/src/pages/product/index.tsx
@@ -1,24 +1,25 @@
-import Link from "next/link"
+import Link from "next/link";
 
-function ProductList(){
-  return <>
-    <div>
-
-    <Link href="/product/1">Product 1</Link>
-    </div>
-    <div>
-
-    <Link href="/product/2">Product 2</Link>
-    </div>
-    <div>
-
-    <Link href="/product/3">Product 3</Link>
-    </div>
-    <div>
-
-    <Link href="/product/4">Product 4</Link>
-    </div>
-  </>
+function ProductList() {
+  return (
+    <>
+      <div>
+        <Link href="/">Home</Link>
+      </div>
+      <div>
+        <Link href="/product/1">Product 1</Link>
+      </div>
+      <div>
+        <Link href="/product/2">Product 2</Link>
+      </div>
+      <div>
+        <Link href="/product/3">Product 3</Link>
+      </div>
+      <div>
+        <Link href="/product/4">Product 4</Link>
+      </div>
+    </>
+  );
 }
 
-export default ProductList
+export default ProductList;
