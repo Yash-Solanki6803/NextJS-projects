640fdf98ced5ba9a1526344f61f5cc43bb0bc360
Yash <thisisyashs@gmail.com>
2023-09-19 18:48:48 +0530

    Navigating using useRouter

--- next-routing/src/pages/product/index.tsx
+++ next-routing/src/pages/product/index.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function ProductList() {
+  const router = useRouter();
   return (
     <>
       <div>
@@ -17,6 +19,15 @@ function ProductList() {
       </div>
       <div>
         <Link href="/product/4">Product 4</Link>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            router.push("/blog/1/review/1");
+          }}
+        >
+          Click me to go to Blogs/1/review/1 page
+        </button>
       </div>
     </>
   );
